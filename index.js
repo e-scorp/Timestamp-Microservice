2b5f5d05f31a8e782526fad7e7793f00ca874b28
Yash doe <yasgavande78@gmail.com>
2024-12-12 17:00:45 +0000

    project

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,8 +21,33 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
+
+app.get("/api/:date?", (req, res,) => {
+  const { date } = req.params
+  let parsedDate
+  if (!date) {
+    // No date provided, use the current date
+    parsedDate = new Date();
+  } else if (!isNaN(date)) {
+    // If date is a valid Unix timestamp
+    parsedDate = new Date(parseInt(date))
+  } else {
+    // Otherwise, parse the date string
+    parsedDate = new Date(date)
+  }
+  // Check if parsed date is valid
+  if (parsedDate.toDateString() === 'Invalid Date') {
+    return res.json({ error: 'Invalid Date' })
+  } 
+
+  //respond with both Unix and Utc formats
+  res.json({
+    unix: parsedDate.getTime(),
+    utc: parsedDate.toUTCString()
+  })
+})
 
 
 
